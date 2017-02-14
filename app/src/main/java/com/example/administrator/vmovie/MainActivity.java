2a280cb56aeea33f27dc8acbcb5654a0d0e2dc1b
KirbyYang951 <kirbyyang951@126.com>
2017-02-14 14:10:38 +0800

    111

--- app/src/main/java/com/example/administrator/vmovie/MainActivity.java
+++ app/src/main/java/com/example/administrator/vmovie/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.administrator.vmovie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//VMovie
 public class MainActivity extends AppCompatActivity {
 
     @Override
